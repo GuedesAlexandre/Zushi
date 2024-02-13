8e3e2e644f19c4ee7f6e641e1c4e88187799418f
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2024-02-13 19:45:58 +0100

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeadertComponent } from './component/headert/headert.component';
 import { HeaderComponent } from './component/header/header.component';
-import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeadertComponent,
     HeaderComponent,
-    FooterComponent
   ],
   imports: [
     BrowserModule,
