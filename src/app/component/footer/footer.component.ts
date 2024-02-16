5753b992a9a3bc1d428a06377f2dc9f3f0e824b3
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2024-02-16 16:08:46 +0100

    test

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -8,3 +8,14 @@ import { Component } from '@angular/core';
 export class FooterComponent {
     
 }
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {
+
+}
