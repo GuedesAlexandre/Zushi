cda5ac9d331fcc4b942393d24438843a5be05372
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2024-02-18 21:14:05 +0100

    ajout fonctionnalité dynamic cart and display

--- src/app/Services/boxes.service.ts
+++ src/app/Services/boxes.service.ts
@@ -10,8 +10,9 @@ export class BoxesService {
   constructor(private http: HttpClient) { }
 
   fetchData(): Observable<any> {
-    const apiUrl = 'https://raw.githubusercontent.com/xtraide/testjson/main/test'; // Remplacez par l'URL de votre API
+    const apiUrl = 'http://localhost:8000/api/boxs'; // Remplacez par l'URL de votre API
 
     return this.http.get(apiUrl);
+    
   }
 }
