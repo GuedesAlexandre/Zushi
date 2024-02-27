51b2466ed9d6277b04902d4e3bb46db567baa411
aroyer1 <arno.royer2004@gmail.com>
2024-02-27 15:59:09 +0100

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './component/landing/landing.component';
 import { ListBoxComponent } from './component/list-box/list-box.component';
-import { LandingComponent } from './component/landing/landing.component';
-import { ListBoxComponent } from './component/list-box/list-box.component';
+
 
 const routes: Routes = [
 {path: '' , component: LandingComponent},
