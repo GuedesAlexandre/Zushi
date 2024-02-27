3fef9f5f6e587c30a3aa59823acc4e8ba637eca5
aroyer1 <arno.royer2004@gmail.com>
2024-02-27 15:26:24 +0100

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
+import { HeaderStaticComponent } from './component/header-static/header-static.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HeaderStaticComponent,
+    FooterComponent,
+    HeaderStaticComponent
   ],
   imports: [
     BrowserModule,
