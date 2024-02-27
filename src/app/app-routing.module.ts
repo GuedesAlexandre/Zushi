279f5b611896c0c3e90a30d754bc72c608a0d920
Eartskip <steven.lecam@outlook.fr>
2024-02-27 15:02:41 +0100

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './component/landing/landing.component';
+import { ListBoxComponent } from './component/list-box/list-box.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '' , component: LandingComponent},
+{path: 'Board' , component: ListBoxComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/component/header-static/header-static.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-static',
+  templateUrl: './header-static.component.html',
+  styleUrls: ['./header-static.component.css']
+})
+export class HeaderStaticComponent {
+
+}
