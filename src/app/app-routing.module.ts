a51500d1427beb7cee38f1adecf4a57650afdeb8
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2024-03-04 11:01:50 +0100

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './component/landing/landing.component';
 import { ListBoxComponent } from './component/list-box/list-box.component';
+import { CommonModule } from '@angular/common';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'Board', component: ListBoxComponent }
-  
+]
 @NgModule({
-  imports: [CommonModule, RouterModule.forRoot(routes)], // Add CommonModule here
+
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
