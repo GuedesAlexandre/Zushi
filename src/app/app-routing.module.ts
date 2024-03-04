00e119c1b6aaf602581ee2c822c1ea269c2e6b1b
Eartskip <steven.lecam@outlook.fr>
2024-03-04 12:13:50 +0100

    About fini

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './component/landing/landing.component';
 import { ListBoxComponent } from './component/list-box/list-box.component';
-import { AboutComponent } from './component/about/about.component';
 
 import { AboutComponent } from './component/about/about.component';
+
+
 import { FoodComponent } from './component/food/food.component';
 
 
