21ddd8fc2ad6b60fa0951fd50978c3ff8822bc40
aroyer1 <arno.royer2004@gmail.com>
2024-03-04 11:24:41 +0100

    Component Hero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { LandingComponent } from './component/landing/landing.component';
 import { __importDefault } from 'tslib'; // Add this import statement
 import { FooterComponent } from './component/footer/footer.component';
 import { HeaderStaticComponent } from './component/header-static/header-static.component';
+import { HeroComponent } from './component/hero/hero.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { HeaderStaticComponent } from './component/header-static/header-static.c
     LandingComponent,
     FooterComponent,
     HeaderStaticComponent,
+    HeroComponent,
     
   ],
   imports: [
